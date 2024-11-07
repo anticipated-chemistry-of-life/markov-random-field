ecdb0f7ca928ca29150c859a8724bde6864a55b8
Marco Visani <92217074+mvisani@users.noreply.github.com>
2024-11-07 12:04:55 +0100

    easy function works ! Still need to check which is faster between the easy function or a basic binary search. Hard function fails for now.

--- tests/TBinary_search_Tests.cpp
+++ tests/TBinary_search_Tests.cpp
@@ -16,7 +16,7 @@ TEST(Binary_search, easy_function) {
 	std::bernoulli_distribution d(p);
 
 	// We create a TStorageYVector object
-	std::vector<size_t> dimensions{100, 200};
+	std::vector<size_t> dimensions{10000, 1000};
 	TStorageYVector Y(1000, dimensions);
 
 	// We fill randomly the TStorageYVector where the values are between 0 and 100*100
@@ -25,6 +25,44 @@ TEST(Binary_search, easy_function) {
 		if (sample) { Y.set_to_one(i); }
 	}
 
-	std::vector<size_t> multi_index{0, 0};
-	auto res = fill_current_state_easy(Y, multi_index, 200);
+	for (size_t j = 0; j < dimensions[0]; ++j) {
+		std::vector<size_t> multi_index_start{j, 0};
+		std::vector<bool> res = fill_current_state_easy(Y, multi_index_start, dimensions[dimensions.size() - 1]);
+		std::vector<bool> truth;
+		truth.clear();
+		auto linear_index = Y.get_linear_coordinate(multi_index_start);
+		for (size_t i = 0; i < dimensions[dimensions.size() - 1]; ++i) { truth.push_back(Y.is_one(linear_index + i)); }
+		EXPECT_EQ(res, truth);
+	}
+}
+
+TEST(Binary_search, hard_function) {
+	// create the engine
+	std::random_device rd{};
+	std::mt19937 gen{rd()};
+
+	double p = 0.5;
+	std::bernoulli_distribution d(p);
+
+	// We create a TStorageYVector object
+	std::vector<size_t> dimensions{4, 6};
+	TStorageYVector Y(1000, dimensions);
+
+	// We fill randomly the TStorageYVector where the values are between 0 and 100*100
+	for (size_t i = 0; i < coretools::containerProduct(dimensions); i++) {
+		auto sample = d(gen);
+		if (sample) { Y.set_to_one(i); }
+	}
+
+	for (auto y : Y.get_vector()) { std::cout << y.get_coordinate() << " "; }
+	for (size_t j = 0; j < dimensions[1]; ++j) {
+		std::vector<size_t> multi_index_start{0, j};
+		std::vector<bool> res = fill_current_state_hard(dimensions[0], Y, multi_index_start, dimensions[1],
+		                                                coretools::containerProduct(dimensions));
+		std::vector<bool> truth;
+		truth.clear();
+		auto linear_index = Y.get_linear_coordinate(multi_index_start);
+		for (size_t i = 0; i < dimensions[0]; ++i) { truth.push_back(Y.is_one(linear_index + i * dimensions[1])); }
+		EXPECT_EQ(res, truth);
+	}
 }
