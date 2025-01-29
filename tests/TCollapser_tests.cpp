a9d0780396e80bff39722885b2a1dc09729a6533
Marco Visani <92217074+mvisani@users.noreply.github.com>
2025-01-29 10:55:31 +0100

    added more unit tests to completly cover x_is_one function

--- tests/TCollapser_tests.cpp
+++ tests/TCollapser_tests.cpp
@@ -13,11 +13,11 @@
 using namespace testing;
 
 TEST(Collapser, keep_1_dim) {
-	TTree tree_1(0, "../tests/test_data/molecules.tsv", "molecules");
-	TTree tree_2(1, "../tests/test_data/loading_tree.tsv", "tissues");
-	TTree tree_3(2, "../tests/test_data/loading_tree.tsv", "species");
+	std::vector<TTree> trees;
 
-	std::vector<TTree> trees = {tree_1, tree_2, tree_3};
+	trees.emplace_back(0, "../tests/test_data/molecules.tsv", "molecules");
+	trees.emplace_back(1, "../tests/test_data/loading_tree.tsv", "tissues");
+	trees.emplace_back(2, "../tests/test_data/loading_tree.tsv", "species");
 
 	TCollapser collapser(trees);
 	std::vector<std::string> dimension_names_to_keep = {"species"};
@@ -26,11 +26,11 @@ TEST(Collapser, keep_1_dim) {
 }
 
 TEST(Collapser, keep_2_dim) {
-	TTree tree_1(0, "../tests/test_data/molecules.tsv", "molecules");
-	TTree tree_2(1, "../tests/test_data/loading_tree.tsv", "tissues");
-	TTree tree_3(2, "../tests/test_data/loading_tree.tsv", "species");
+	std::vector<TTree> trees;
 
-	std::vector<TTree> trees = {tree_1, tree_2, tree_3};
+	trees.emplace_back(0, "../tests/test_data/molecules.tsv", "molecules");
+	trees.emplace_back(1, "../tests/test_data/loading_tree.tsv", "tissues");
+	trees.emplace_back(2, "../tests/test_data/loading_tree.tsv", "species");
 
 	TCollapser collapser(trees);
 	std::vector<std::string> dimension_names_to_keep = {"molecules", "species"};
@@ -63,6 +63,8 @@ TEST(Collapser, counter) {
 }
 
 TEST(Collapser, check_if_other_dims_are_not_collapsed) {
+	/// this test checks that if the collapser collapses a specific dimension, then
+	/// checking if x_is_one in other dimensions should return false
 	std::vector<TTree> trees;
 
 	trees.emplace_back(0, "../tests/test_data/molecules.tsv", "molecules");
@@ -99,6 +101,78 @@ TEST(Collapser, old_state) {
 
 	TCollapser collapser(trees);
 	std::vector<std::string> dimension_names_to_keep = {"molecules", "species"};
+	collapser.initialize(dimension_names_to_keep, "lotus");
+
+	EXPECT_EQ(collapser.x_is_one({0, 1, 0}, true), false);
+	EXPECT_EQ(collapser.x_is_one({0, 1, 0}, false), true);
+}
+
+TEST(Collapser, old_state_is_0) {
+	std::vector<TTree> trees;
+
+	trees.emplace_back(0, "../tests/test_data/molecules.tsv", "molecules");
+	trees.emplace_back(1, "../tests/test_data/loading_tree.tsv", "tissues");
+	trees.emplace_back(2, "../tests/test_data/loading_tree.tsv", "species");
+
+	for (auto &tree : trees) { tree.initialize_cliques_and_Z(trees); }
+	trees[1].get_clique({0, 0, 0}).update_counter_leaves_state_1(0);
+
+	TCollapser collapser(trees);
+	std::vector<std::string> dimension_names_to_keep = {"molecules", "species"};
+	collapser.initialize(dimension_names_to_keep, "lotus");
+
+	EXPECT_EQ(collapser.x_is_one({0, 1, 0}, true), false);
+	EXPECT_EQ(collapser.x_is_one({0, 1, 0}, false), false);
+}
+
+TEST(Collapser, old_state_is_bigger_than_1) {
+	std::vector<TTree> trees;
+
+	trees.emplace_back(0, "../tests/test_data/molecules.tsv", "molecules");
+	trees.emplace_back(1, "../tests/test_data/loading_tree.tsv", "tissues");
+	trees.emplace_back(2, "../tests/test_data/loading_tree.tsv", "species");
+
+	for (auto &tree : trees) { tree.initialize_cliques_and_Z(trees); }
+	trees[1].get_clique({0, 0, 0}).update_counter_leaves_state_1(10);
+
+	TCollapser collapser(trees);
+	std::vector<std::string> dimension_names_to_keep = {"molecules", "species"};
+	collapser.initialize(dimension_names_to_keep, "lotus");
+
+	EXPECT_EQ(collapser.x_is_one({0, 1, 0}, true), true);
+	EXPECT_EQ(collapser.x_is_one({0, 1, 0}, false), true);
+}
+
+TEST(Collapser, collpase_2_dims) {
+	std::vector<TTree> trees;
+
+	trees.emplace_back(0, "../tests/test_data/molecules.tsv", "molecules");
+	trees.emplace_back(1, "../tests/test_data/loading_tree.tsv", "tissues");
+	trees.emplace_back(2, "../tests/test_data/loading_tree.tsv", "species");
+
+	for (auto &tree : trees) { tree.initialize_cliques_and_Z(trees); }
+	trees[1].get_clique({0, 0, 0}).update_counter_leaves_state_1(10);
+
+	TCollapser collapser(trees);
+	std::vector<std::string> dimension_names_to_keep = {"species"};
+	collapser.initialize(dimension_names_to_keep, "lotus");
+
+	EXPECT_EQ(collapser.x_is_one({0, 1, 0}, true), true);
+	EXPECT_EQ(collapser.x_is_one({0, 1, 0}, false), true);
+}
+
+TEST(Collapser, collpase_2_dims_and_check_old_state_is_1) {
+	std::vector<TTree> trees;
+
+	trees.emplace_back(0, "../tests/test_data/molecules.tsv", "molecules");
+	trees.emplace_back(1, "../tests/test_data/loading_tree.tsv", "tissues");
+	trees.emplace_back(2, "../tests/test_data/loading_tree.tsv", "species");
+
+	for (auto &tree : trees) { tree.initialize_cliques_and_Z(trees); }
+	trees[1].get_clique({0, 0, 0}).update_counter_leaves_state_1(1);
+
+	TCollapser collapser(trees);
+	std::vector<std::string> dimension_names_to_keep = {"species"};
 	collapser.initialize(dimension_names_to_keep, "lotus");
 
 	EXPECT_EQ(collapser.x_is_one({0, 1, 0}, true), false);
