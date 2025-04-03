f0752e38532755a2d3ccb829002ab563410ae6b2
Marco Visani <92217074+mvisani@users.noreply.github.com>
2025-04-03 07:48:40 +0200

    added generation of infer.sh file automatically

--- model_validation/generate_data.py
+++ model_validation/generate_data.py
@@ -39,7 +39,51 @@ def create_simulate_file(
     --write_Y_trace \\
     --write_Z_trace \\
     --fixedSeed 42 \\
-        """
+ """
+
+
+def create_infer_file(
+    species_tree_name: str,
+    molecules_tree_name: str,
+) -> str:
+    return f"""
+../../build/acol infer \
+    --out ./test_out/acol \
+    --tree_species {species_tree_name}.txt \
+    --tree_molecules {molecules_tree_name}.txt \
+    --lotus acol_simulated_lotus.tsv \
+    --iterations 20000 \
+    --numThreads 1 \
+    --writeTrace \
+    --writeBurnin \
+    --write_Y_trace \
+    --set_Y acol_simulated_Y.txt \
+    --Y.update false \
+    --write_Z_trace \
+    --Z.update false \
+    --set_species_Z acol_simulated_Z_species.txt \
+    --set_molecules_Z acol_simulated_Z_molecules.txt \
+    --molecules_branch_lengths acol_input_simulated.txt \
+    --molecules_branch_lengths.update false \
+    --molecules_log_nu acol_input_simulated.txt \
+    --molecules_log_nu.update false \
+    --molecules_mean_log_nu acol_input_simulated.txt \
+    --molecules_mean_log_nu.update false \
+    --molecules_var_log_nu acol_input_simulated.txt \
+    --molecules_var_log_nu.update false \
+    --molecules_alpha acol_input_simulated.txt \
+    --molecules_alpha.update false \
+    --species_branch_lengths acol_input_simulated.txt \
+    --species_branch_lengths.update false \
+    --species_var_log_nu acol_input_simulated.txt \
+    --species_var_log_nu.update false \
+    --species_mean_log_nu acol_input_simulated.txt \
+    --species_mean_log_nu.update false \
+    --species_log_nu acol_input_simulated.txt \
+    --species_log_nu.update false \
+    --species_alpha acol_input_simulated.txt \
+    # --species_alpha.update false \
+"""
 
 
 def main():
@@ -114,6 +158,18 @@ def main():
     )
     with open(os.path.join(args.out, "simulate.sh"), "w") as f:
         f.write(sim_file)
+
+    # now we create the diretory "test_out" for the output of inference
+    if not os.path.exists(os.path.join(args.out, "test_out")):
+        os.makedirs(os.path.join(args.out, "test_out"))
+
+    # Write the infer file
+    inf_file = create_infer_file(
+        species_tree_name=args.species_tree_name,
+        molecules_tree_name=args.molecules_tree_name,
+    )
+    with open(os.path.join(args.out, "infer.sh"), "w") as f:
+        f.write(inf_file)
 
 
 if __name__ == "__main__":
