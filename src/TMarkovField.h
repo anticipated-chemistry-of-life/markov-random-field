8a0df0a7de9d2797c5aafa5e7bb7dc4b722438bc
Marco Visani <92217074+mvisani@users.noreply.github.com>
2025-07-25 12:43:46 +0200

    more debug

--- src/TMarkovField.h
+++ src/TMarkovField.h
@@ -137,12 +137,14 @@ private:
 		// loop over sheets in last dimension
 		std::vector<coretools::TSumLogProbability> new_LL(NUMBER_OF_THREADS);
 
+		OUT("There are ", _num_outer_loops, "outer loops.")
 		for (size_t k = 0; k < _num_outer_loops; ++k) {
 			const size_t start_ix_in_leaves_last_dim = k * _K; // 0, _K, 2*_K, ...
 
 			// loop over all dimensions except last (linearized)
 			size_t num_inner_loops = coretools::containerProduct(_num_leaves_per_dim_except_last);
 			std::vector<size_t> previous_ix;
+			OUT("there are ", num_inner_loops, " inner loops.")
 			for (size_t i = 0; i < num_inner_loops; ++i) {
 				// get multi-dimensional index from linear coordinate and set the start of the last dimension
 				auto start_index_in_leaves_space   = coretools::getSubscripts(i, _num_leaves_per_dim_except_last);
@@ -174,6 +176,7 @@ private:
 				// insert new 1-valued indices into Y
 				// Note: indices of where Y is one in _sheets is not accurate anymore, but we don't use them, so it's ok
 				_Y.insert_in_Y(linear_indices_in_Y_space_to_insert);
+				OUT("done inserting");
 				_trees.back()
 				    ->get_clique(start_index_in_leaves_space)
 				    .update_counter_leaves_state_1(diff_counter_1_in_last_dim);
