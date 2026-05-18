4973868577df1c15a179139db41561000f2dd6ce
Marco Visani <92217074+mvisani@users.noreply.github.com>
2026-05-18 07:37:20 +0200

    added writing of compiler commands

--- xmake.lua
+++ xmake.lua
@@ -6,6 +6,7 @@ add_rules("mode.debug", "mode.release")
 set_languages("c++20")
 
 set_policy("generator.compile_commands", true)
+add_rules("plugin.compile_commands.autoupdate", { outputdir = "build" })
 
 
 
