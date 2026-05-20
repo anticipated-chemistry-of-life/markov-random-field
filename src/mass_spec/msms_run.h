f792d7472e6ced248663479eea65446c11b8b633
Marco Visani <92217074+mvisani@users.noreply.github.com>
2026-05-20 14:11:30 +0200

    forgot to add include <optional> in header so compilation was failing in Linux

--- src/mass_spec/msms_run.h
+++ src/mass_spec/msms_run.h
@@ -1,9 +1,11 @@
 #pragma once
 
 /// Class storing a mass spectrometry run. The iterator goes along the
-/// molecule dimension. So for each feature, we have a list of molecule indices and binned likelihoods.
+/// molecule dimension. So for each feature, we have a list of molecule indices and binned
+/// likelihoods.
 #include "./feature_likelihood.h"
 #include "coretools/Containers/TNestedVector.h"
+#include <optional>
 
 struct BinarySearchResult {
 	bool found{};
@@ -22,7 +24,8 @@ public:
 	[[nodiscard]] bool empty() const { return _features.empty(); }
 	void add_empty_feature() { _features.push_back(); }
 
-	[[nodiscard]] coretools::TConstView<TFeatureLikelihood> get_likelihoods_for_feature(size_t i) const {
+	[[nodiscard]] coretools::TConstView<TFeatureLikelihood>
+	get_likelihoods_for_feature(size_t i) const {
 		return _features.get(i);
 	}
 	[[nodiscard]] size_t size() const { return _features.size(); }
@@ -30,13 +33,15 @@ public:
 	[[nodiscard]] auto end() const { return _features.end(); }
 	auto begin() { return _features.begin(); }
 	auto end() { return _features.end(); }
-	[[nodiscard]] BinarySearchResult is_molecule_in_feature(size_t feature_idx, uint32_t molecule_index) const {
+	[[nodiscard]] BinarySearchResult is_molecule_in_feature(size_t feature_idx,
+	                                                        uint32_t molecule_index) const {
 		const auto &likelihoods = _features.get(feature_idx);
 		return is_molecule_in_feature(likelihoods, molecule_index);
 	}
 
-	inline static BinarySearchResult is_molecule_in_feature(const coretools::TConstView<TFeatureLikelihood> &feature,
-	                                                        uint32_t molecule_index) {
+	inline static BinarySearchResult
+	is_molecule_in_feature(const coretools::TConstView<TFeatureLikelihood> &feature,
+	                       uint32_t molecule_index) {
 		auto it = std::lower_bound(feature.begin(), feature.end(), molecule_index);
 		if (it == feature.end()) { return {false, std::nullopt, feature.size()}; }
 		size_t index = std::distance(feature.begin(), it);
@@ -44,14 +49,15 @@ public:
 		return {true, it->get_binned_likelihood(), index};
 	}
 
-	static double get_likelihood_from_binned_value(const std::array<double, 256> &binned_likelihoods,
-	                                               uint8_t binned_value) {
+	static double
+	get_likelihood_from_binned_value(const std::array<double, 256> &binned_likelihoods,
+	                                 uint8_t binned_value) {
 		return binned_likelihoods[binned_value];
 	}
 
-	/// Adds a vector of molecule likelihoods. This represents one feature with all the molecule likelihoods that are
-	/// associated to it. The vector will be sorted before being added to guarantee the order and be able to do binary
-	/// search.
+	/// Adds a vector of molecule likelihoods. This represents one feature with all the molecule
+	/// likelihoods that are associated to it. The vector will be sorted before being added to
+	/// guarantee the order and be able to do binary search.
 	void add_likelihood_vector(std::vector<TFeatureLikelihood> &feature_likelihoods) {
 		std::sort(feature_likelihoods.begin(), feature_likelihoods.end());
 		_features.push_back(feature_likelihoods);
